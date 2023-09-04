9c4b505c8bbc7a90d052f7c4137833363d75c258
Franco Callegari <francocallegari12@gmail.com>
2023-09-04 15:43:24 -0300

    Agregada función nuevo_libro y generar_codigo

--- libro.py
+++ libro.py
@@ -1,3 +1,5 @@
+import cod_generator
+
 # Crear un diccionario para cada libro
 libro1 = {'cod': 'CRBJsAkS', 'cant_ej_ad': 3, 'cant_ej_pr': 1, "titulo": "Cien años de soledad", "autor": "Gabriel García Márquez"}
 libro2 = {'cod': 'QgfV4j3c', 'cant_ej_ad': 4, 'cant_ej_pr': 2, "titulo": "El principito", "autor": "Antoine de Saint-Exupéry"}
@@ -5,8 +7,15 @@ libro3 = {'cod': 'adOd09UE', 'cant_ej_ad': 1, 'cant_ej_pr': 0, "titulo": "El có
 
 def nuevo_libro():
     #completar
-    return None
+    cod = generar_codigo()
+    cant_ej_ad = input("Ingrese la cantidad de ejemplares adquiridos: ")
+    titulo = input("Ingrese el título del libro: ")
+    autor = input("Ingrese el autor del libro: ")
+
+    agregar_libro = {'cod': cod, 'cant_ej_ad': cant_ej_ad, 'cant_ej_pr': 0, "titulo": titulo, "autor": autor}
+    return agregar_libro
 
 def generar_codigo():
     #completar
-    return None
+    cod = cod_generator.generar()
+    return cod
